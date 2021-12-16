e2200ac256df14b400106f678c2a196d1a33a249
handrifirdaus <firdaushandri88@gmail.com>
2021-12-16 17:09:28 +0700

    coba react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+
+
+import axios from 'axios'
+
+const Login = () => {
+  const tes = axios
+  return (
+    <div className='open'>
+      coba
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar">
+        <div className="navbar-container">
+          <p>test</p>
+          <Login />
+          <div class="navbar-brand">
+            <a href="index.html">
+              <img src="img/cart.png" width="50" height="50"></img>
+              <a>Food&Drink</a>
+            </a>
+
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
