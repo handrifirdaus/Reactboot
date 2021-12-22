326b3b3bcb80c49179a86405e2101796f78c1858
handrifirdaus <firdaushandri88@gmail.com>
2021-12-22 17:42:35 +0700

    first react

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
 
 import './App.css';
+import Img from './img/cart.png';
+import Img1 from './img/marker.png';
 import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 
 
 import axios from 'axios'
 
-const Login = () => {
-  const tes = axios
-  return (
-    <div className='open'>
-      coba
-    </div>
-  )
-}
+// const Login = () => {
+//   const tes = axios
+//   return (
+//     <div className='open'>
+//       coba
+//     </div>
+//   )
+// }
 function App() {
   return (
-    <div className="App">
-      <nav class="navbar">
-        <div className="navbar-container">
-          <p>test</p>
-          <Login />
-          <div class="navbar-brand">
-            <a href="index.html">
-              <img src="img/cart.png" width="50" height="50"></img>
-              <a>Food&Drink</a>
-            </a>
-
-          </div>
+    <nav className="navbar">
+      <div class="navbar-container">
+        <div class="navbar-brand">
+          <a href="index.html">
+            <img src={Img} width="50" height="50" />
+            <a>Food&Drink</a>
+          </a>
         </div>
-      </nav>
-    </div>
-  );
-}
 
+        <ul class="navbar-nav-left">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Menu</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Rewards</a>
+          </li>
+          <li>
+            <a href="#">Message</a>
+          </li>
+          <li>
+            <a href="#">Saran</a>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav-right">
+          <li>
+            <a href="#">
+              <img src={Img1} width="50" height="50"></img>
+              <span>Location a store </span>
+            </a>
+          </li>
+          <li>
+            <button class="btn btn-dark-outline">sign in</button>
+            <button class="btn btn-dark">join now</button>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+  )
+};
 export default App;
+
